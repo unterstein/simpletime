cca8d23af69ffc59c5cd4b22bd654fc83f614674
Johannes Unterstein <unterstein@me.com>
2015-12-10 23:10:29 +0100

    moved code, added deps

--- web/backend/app/logic/Global.scala
+++ web/backend/app/logic/Global.scala
@@ -1,8 +1,41 @@
 package logic
 
+import _root_.neo4jplugin.Neo4jPlugin
+import neo4jplugin.Neo4jPlugin
+import java.util
+import java.util.concurrent.Callable
+
+import play.inject.ApplicationLifecycle
+import play.libs.F.Promise
+
+import play.api.{Mode, Application, GlobalSettings}
+import scala.collection.JavaConversions._
+
+
 /**
  * @author Johannes Unterstein (unterstein@me.com)
  */
-object Global {
+object Global extends GlobalSettings {
+
+  private val hooks = new util.ArrayList[Callable[Promise[Void]]]()
+
+  private var app: Application = null
+
+  def isDev: Boolean = app.mode == Mode.Dev
+
+  override def onStart(app: Application): Unit = {
+    super.onStart(app)
+    this.app = app
+    new Neo4jPlugin(new ApplicationLifecycle {
+      override def addStopHook(hook: Callable[Promise[Void]]): Unit = hooks.add(hook)
+    })
+
+  }
+
+  override def onStop(app: Application): Unit = {
+    super.onStop(app)
+
+    hooks.map(f => f.call())
+  }
 
 }
--- web/backend/build.sbt
+++ web/backend/build.sbt
@@ -1,3 +1,7 @@
+import play.sbt.PlayImport._
+import play.sbt.PlayScala
+import play.sbt.routes.RoutesKeys._
+
 name := """simpletime"""
 
 version := "1.0-SNAPSHOT"
@@ -10,10 +14,15 @@ libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
-  specs2 % Test
+  specs2 % Test,
+  "com.github.tuxBurner" %% "play-neo4jplugin" % "1.5.0",
+  "com.google.code.gson" % "gson" % "2.4"
 )
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+resolvers += "tuxburner.github.io" at "http://tuxburner.github.io/repo"
+resolvers += "Neo4j Maven Repo" at "http://m2.neo4j.org/content/repositories/releases"
+resolvers += "Spring milestones" at "http://repo.spring.io/milestone"
 
 // Play provides two styles of routers, one expects its actions to be injected, the
 // other, legacy style, accesses its actions statically.
