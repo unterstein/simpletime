f464bc1dc652c2ada027beadf044dee6b59bf7d6
Johannes Unterstein <unterstein@me.com>
2015-12-31 16:26:41 +0100

    removed inline js code

---
+++ web-app/public/js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+  var datamodel = $("#datamodel");
+  var model = datamodel.data("model");
+  var column = datamodel.data("column");
+  var viewModel = ko.mapping.fromJS(model);
+
+  ko.applyBindings(viewModel);
+
+  $("#newColumn").click(function () {
+    viewModel.columns.push(cloneObservable(column));
+    return false;
+  });
+
+  $(document).on("click", ".deleteColumn", function () {
+    var removeItem = $(".deleteColumn").index(this);
+    console.log(removeItem);
+    return false;
+  });
+
+  function cloneObservable(observableObject) {
+    return ko.mapping.fromJS(ko.toJS(observableObject));
+  }
+});
