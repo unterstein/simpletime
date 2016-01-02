c855e055c9426f240d3abacd0b5e448ead93508c
Johannes Unterstein <unterstein@me.com>
2016-01-02 23:43:51 +0100

    save posted project

--- web-app/app/controllers/ProjectController.scala
+++ web-app/app/controllers/ProjectController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject.Inject
 
 import controllers.ProjectController.CaseProject
+import logic.HashHelper
 import neo4j.models.project.{ProjectColumn, Project, ProjectColumnType}
 import neo4j.services.Neo4jProvider
 import play.api.data.Form
@@ -25,11 +26,11 @@ class ProjectController @Inject()(messages: MessagesApi) extends BaseController 
         formWithErrors => Ok(views.html.projectEdit(hash, formWithErrors, ProjectController.initialColumn)),
         value => {
           // TODO remove "new" hack
-          if ("new".equals("hash")) {
+          if ("new".equals(hash)) {
             val project = new Project
             project.name = value.name
-            project.id = value.id
             project.user = request.user
+            project.hash = HashHelper.uuid()
             project.columns = value.columns.map {
               column =>
                 val projectColumn = new ProjectColumn
@@ -38,10 +39,11 @@ class ProjectController @Inject()(messages: MessagesApi) extends BaseController 
                 projectColumn
             }.toList
             Neo4jProvider.get().projectRepository.save(project)
+            Redirect(routes.ProjectController.edit(project.hash))
           } else {
             // edit
+            Redirect(routes.ProjectController.edit(hash))
           }
-          Ok("")
         }
       )
   }
@@ -54,7 +56,7 @@ class ProjectController @Inject()(messages: MessagesApi) extends BaseController 
   def edit(hash: String) = AuthenticatedBaseAction {
     implicit request =>
       // TODO load from database
-      Ok(views.html.projectEdit(hash, ProjectController.projectForm.fill(CaseProject(-1L, "TODO", List())), ProjectController.initialColumn))
+      Ok(views.html.projectEdit(hash, ProjectController.projectForm.fill(CaseProject("TODO", List())), ProjectController.initialColumn))
   }
 
   override def messagesApi: MessagesApi = messages
@@ -62,31 +64,30 @@ class ProjectController @Inject()(messages: MessagesApi) extends BaseController 
 
 object ProjectController {
 
-  case class CaseColumn(columnId: Long, columnName: String, columnType: String) {
+  case class CaseColumn(columnId: Option[Long], columnName: String, columnType: String) {
     val columnTypes = ProjectColumnType.values().map { e => e.name}.toList.asJava
   }
 
-  case class CaseProject(id: Long, name: String, columns: List[CaseColumn])
+  case class CaseProject(name: String, columns: List[CaseColumn])
 
   val projectForm: Form[CaseProject] = Form(
     mapping(
-      "id" -> longNumber,
       "name" -> nonEmptyText,
       "columns" -> list(mapping(
-        "columnId" -> longNumber,
+        "columnId" -> optional(longNumber),
         "columnName" -> nonEmptyText,
         "columnType" -> nonEmptyText
       )(CaseColumn.apply)(CaseColumn.unapply))
     )(CaseProject.apply)(CaseProject.unapply)
   )
 
-  def initialColumn()(implicit messages: play.api.i18n.Messages) = CaseColumn(-1L, "", ProjectColumnType.STRING.name())
+  def initialColumn()(implicit messages: play.api.i18n.Messages) = CaseColumn(None, "", ProjectColumnType.STRING.name())
 
   def initialProjectForm()(implicit messages: play.api.i18n.Messages): Form[CaseProject] = {
     val columns = List(
-      CaseColumn(-1L, Messages("description"), ProjectColumnType.STRING.name()),
-      CaseColumn(-1L, Messages("planned"), ProjectColumnType.BOOLEAN.name())
+      CaseColumn(None, Messages("description"), ProjectColumnType.STRING.name()),
+      CaseColumn(None, Messages("planned"), ProjectColumnType.BOOLEAN.name())
     )
-    projectForm.fill(CaseProject(-1L, Messages("project.default"), columns))
+    projectForm.fill(CaseProject(Messages("project.default"), columns))
   }
 }
