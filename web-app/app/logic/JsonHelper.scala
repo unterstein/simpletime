c9579ea40229fd128d976e31cb38b0d14e447779
Johannes Unterstein <unterstein@me.com>
2016-01-31 10:10:43 +0100

    removed doc

--- web-app/app/logic/JsonHelper.scala
+++ web-app/app/logic/JsonHelper.scala
@@ -35,7 +35,6 @@ object JsonHelper {
       if (src.value.isDefined) {
         gson.toJsonTree(src.get)
       } else {
-        // fuck
         // TODO, maybe create new form without constraints?
         gson.toJsonTree(src.data.asJava)
       }
